63e0ff5f23ac0f79780514396201828eb1f2670d
rokoDev <mishaDoter@gmail.com>
2020-10-11 15:22:55 +0300

    CLIENT-1 made version and git info available from source code

--- tests/UDPClient_tests.cpp
+++ tests/UDPClient_tests.cpp
@@ -6,10 +6,29 @@
 //  Copyright © 2016 roko. All rights reserved.
 //
 
+#include "GitInfo.h"
 #include "UDPClient.h"
+#include "Version.h"
 #include "gtest/gtest.h"
 
-TEST(TestGroup1, Test1) { ASSERT_EQ(0, 0); }
+TEST(TestUDPClientVersion, GitInfoTest)
+{
+    std::cout << "[          ] branch: " << UDPClient::GitInfo::branch()
+              << '\n';
+    std::cout << "[          ] SHA1: " << UDPClient::GitInfo::SHA1() << '\n';
+    const std::string isDirty = UDPClient::GitInfo::isDirty()
+                                    ? std::string("true")
+                                    : std::string("false");
+    std::cout << "[          ] IS_DIRTY: " << isDirty << std::endl;
+    ASSERT_EQ(0, 0);
+}
+
+TEST(TestUDPClientVersion, VersionTest)
+{
+    std::cout << "[          ] UDPClient Version: " << UDPClient::Version::str()
+              << std::endl;
+    ASSERT_EQ(0, 0);
+}
 
 int main(int argc, char **argv)
 {
